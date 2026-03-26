c2fd06d64f706ff60b50e515b0134e3c24a8ec28
Talandar <talandar66@protonmail.com>
2026-03-26 08:16:30 +0100

    abutobarreling blacklist

--- data.lua
+++ data.lua
@@ -1,6 +1,3 @@
-local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
-local item_effects = require("__space-age__.prototypes.item-effects")
-local item_tints = require("__base__.prototypes.item-tints")
 local item_sounds = require("__base__.prototypes.item_sounds")
 local hit_effects = require("__base__.prototypes.entity.hit-effects")
 local sounds = require("__base__.prototypes.entity.sounds")
